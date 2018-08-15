25d3a9792285eefdf3b787c5417fb7d9668f5669
Marcus <marcus.kerr94@gmail.com>
2018-08-15 14:07:56 +0100

    Deleted unnecessary tests

--- cypress/integration/add_listing.spec.js
+++ cypress/integration/add_listing.spec.js
@@ -1,13 +1,16 @@
 describe("add a listing", function(){
-  it('fils in fields', function(){
+  it('fills in fields', function(){
     cy.visit('/index.html')
-      .get('#listingTitle')
-      .type('Chimichanga man')
-      .get('#listingPhoneNumber')
+      .get('#propertyTitle')
+      .type('Chimichanga Man')
+      .get('#propertyPhoneNumber')
       .type('0000')
-    cy.get('#submitButton')
+      .get('#propertyDescription')
+      .type("This property is the bee's knees")
+      cy.get('#addPropertyButton')
       .click()
     cy.contains('Chimichanga man')
+    cy.contains("This property is the bee's knees")
     cy.contains('0000')
   });
 });
