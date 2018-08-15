32a73d02277d2f57d173cb0844d2629f24ab1580
edthomas93 <et1270@my.bristol.ac.uk>
2018-08-15 11:16:37 +0100

    Created index.html and style.css files

--- spec/ListingSpec.js
+++ spec/ListingSpec.js
@@ -6,6 +6,17 @@ describe("Property", function(){
     property = new Property();
   });
 
+  describe("#getAll", function(){
+    it('lists all the properties', function(){
+      property.add('Buckingham Palace', 999);
+      property.add('Ed\'s Palace', 911);
+      expect(property.getAll()[0]["title"]).toEqual("Buckingham Palace");
+      expect(property.getAll()[0]["number"]).toEqual("999");
+      expect(property.getAll()[1]["title"]).toEqual("Ed's Palace");
+      expect(property.getAll()[1]["number"]).toEqual("911");
+    });
+  });
+
   describe('#add', function(){
     it('adds a property to the database', function(){
       property.add('Buckingham Palace', 999);
--- src/Listing.js
+++ src/Listing.js
@@ -2,6 +2,10 @@ function Property(){
   this.all = [];
 };
 
+Property.prototype.getAll = function(){
+  return this.all
+};
+
 Property.prototype.add = function(mytitle, mynumber) {
   this.all.push({title: `${mytitle}`, number: `${mynumber}`})
 };
