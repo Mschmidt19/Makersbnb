dc4af18f96dbca408e0aaa2c7095ec9fd4747a20
Marek <MarekSchmidt@outlook.com>
2018-08-15 19:02:50 +0100

    deleting from database works, but live update doesn't

--- src/property.js
+++ src/property.js
@@ -5,17 +5,38 @@ firestore.settings(settings);
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
 
+var propertyContainer = document.getElementById("propertyContainer");
+
+db.collection("Listings").onSnapshot(function(querySnapshot) {
+
+    querySnapshot.docChanges().forEach(function(change) {
+
+      if (change.type === "added") {
+
+        propertyContainer.innerHTML += "<div class='" + change.doc.id + " listing'><div class='innertext'>" + change.doc.data().title + "<br>Contact number: " + change.doc.data().phone_number + "<br>" + change.doc.data().description + "<br><button onClick='bookProperty(this.id)' id='" + change.doc.id + "'>Book</button></div><div class='innerimage'><img class='image' src='https://metrouk2.files.wordpress.com/2016/07/ad_212386187.jpg?quality=80&strip=all&strip=all'></div></div>"
+
+      }
+      else if (change.type === "removed") { // doesn't work yet
+        itemToRemove = document.getElementsByClassName(change.doc.id);
+        console.log(itemToRemove);
+        propertyContainer.removeChild(itemToRemove);
+      }
+
+    });
+
+});
+
 function addProperty() {
 
   var titleText = document.getElementById("propertyTitle").value;
   var phoneNumber = document.getElementById("propertyPhoneNumber").value;
   var description = document.getElementById("propertyDescription").value;
 
-
   db.collection("Listings").doc().set({
       title: titleText,
       phone_number: phoneNumber,
-      description: description
+      description: description,
+
   })
   .then(function() {
       console.log("Listing successfully written!");
@@ -26,18 +47,12 @@ function addProperty() {
 
 }
 
-var propertyContainer = document.getElementById("propertyContainer");
+function bookProperty(button_id) {
 
-db.collection("Listings").onSnapshot(function(querySnapshot) {
+  db.collection("Listings").doc(button_id).delete().then(function() {
+    console.log("Document successfully deleted!");
+  }).catch(function(error) {
+    console.error("Error removing document: ", error);
+  });
 
-    querySnapshot.docChanges().forEach(function(change) {
-
-      if(change.type === "added") {
-
-        propertyContainer.innerHTML += "<div class='listing'><div class='innertext'>" + change.doc.data().title + "<br>Contact number: " + change.doc.data().phone_number + "<br>" + change.doc.data().description + "</div><div class='innerimage'><img class='image' src='https://metrouk2.files.wordpress.com/2016/07/ad_212386187.jpg?quality=80&strip=all&strip=all'></div></div>"
-
-      }
-
-    });
-
-});
+}
