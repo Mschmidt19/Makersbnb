08a297eec15cc15bbf876afd668ee43ff12260d3
Marcus <marcus.kerr94@gmail.com>
2018-08-15 14:39:39 +0100

    Cypress integration, first feature test complete

--- cypress/integration/add_listing.spec.js
+++ cypress/integration/add_listing.spec.js
@@ -2,14 +2,14 @@ describe("add a listing", function(){
   it('fills in fields', function(){
     cy.visit('/index.html')
       .get('#propertyTitle')
-      .type('Chimichanga Man')
+      .type('Taco Man')
       .get('#propertyPhoneNumber')
       .type('0000')
       .get('#propertyDescription')
       .type("This property is the bee's knees")
     cy.get('#addPropertyButton')
       .click()
-    cy.contains('Chimichanga man')
+    cy.contains('Taco Man')
     cy.contains("This property is the bee's knees")
     cy.contains('0000')
   });
