c2b2ed7efe56b6e0586b738c00d718398645134e
edthomas93 <et1270@my.bristol.ac.uk>
2018-08-15 17:33:22 +0100

    Added image functionality via URLS

--- src/property.js
+++ src/property.js
@@ -10,11 +10,13 @@ function addProperty() {
   var titleText = document.getElementById("propertyTitle").value;
   var phoneNumber = document.getElementById("propertyPhoneNumber").value;
   var description = document.getElementById("propertyDescription").value;
+  var image = document.getElementById("propertyImage").value;
 
 
   db.collection("Listings").doc().set({
       title: titleText,
       phone_number: phoneNumber,
+      image: image,
       description: description
   })
   .then(function() {
@@ -34,7 +36,7 @@ db.collection("Listings").onSnapshot(function(querySnapshot) {
 
       if(change.type === "added") {
 
-        propertyContainer.innerHTML += "<div class='listing'><div class='innertext'>" + change.doc.data().title + "<br>Contact number: " + change.doc.data().phone_number + "<br>" + change.doc.data().description + "</div><div class='innerimage'><img class='image' src='https://metrouk2.files.wordpress.com/2016/07/ad_212386187.jpg?quality=80&strip=all&strip=all'></div></div>"
+        propertyContainer.innerHTML += "<div class='listing'><div class='innertext'>" + change.doc.data().title + "<br>Contact number: " + change.doc.data().phone_number + "<br>" + change.doc.data().description + "</div><div class='innerimage'><img class='image' src=" + change.doc.data().image + "></div></div>"
 
       }
 
